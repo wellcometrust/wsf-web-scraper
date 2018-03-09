a322ce269232ff8f3580e7a0b288bc599f1d68cf
Samuel Depardieu <6536364+SamDepardieu@users.noreply.github.com>
2018-03-09 10:16:23 +0000

    Changes the way to check for duplicates by using the file footprint (#49)
    * Changes the way to check for duplicates by using the file hash.
    This way prevents us from ignoring an updated file, and also allow us to check if a file has a different title but the same content.
    
    * Changes the file_hash field size, as it will never be more than 32 chars
    
    * Rewrite dbtools for a better database and error management.
    
    * Remove string interpolation from loggers
    
    * Rewrite file hash function
    
    * Add dbTools testing and raise the excetion on _execute
    
    * Changes path generations, now uses os.path

---
+++ tests/test_db_tools.py
@@ -0,0 +1,29 @@
+import unittest
+import sqlite3
+from tools import SQLite3Connector
+
+
+class TestDBTools(unittest.TestCase):
+
+    def setUp(self):
+        self.database = SQLite3Connector()
+
+    def test_queries(self):
+        self.database.insert_article(
+            'Test',
+            '0' * 32,
+            'http://example.com/pdf.pdf'
+        )
+        self.assertTrue(self.database.is_scraped('0' * 32))
+        self.database._execute(
+            'DELETE FROM article WHERE file_hash = ?',
+            ('0' * 32,)
+        )
+        self.assertFalse(self.database.is_scraped('0' * 32))
+
+    def test_exception(self):
+            self.assertRaises(
+                sqlite3.Error,
+                self.database._execute,
+                'SELECT 1 FROM sssss'
+            )
--- wsf_scraping/pipelines.py
+++ wsf_scraping/pipelines.py
@@ -2,9 +2,10 @@
 import os
 import logging
 from scrapy import spiderloader
+from tools import SQLite3Connector
+from tools.cleaners import parse_keywords_files, get_file_hash
 from scrapy.utils.project import get_project_settings
-from tools.dbTools import insert_article
-from tools.cleaners import parse_keywords_files
+from scrapy.exceptions import DropItem
 from pdf_parser.pdf_parse import (parse_pdf_document, grab_section,
                                   parse_pdf_document_pdftxt)
 
@@ -25,63 +26,69 @@ class WsfScrapingPipeline(object):
 
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
+        self.database = SQLite3Connector()
 
         spider_loader = spiderloader.SpiderLoader.from_settings(self.settings)
         spiders = spider_loader.list()
 
         for spider_name in spiders:
-            os.makedirs('./results/pdfs/%s' % spider_name, exist_ok=True)
+            folder_path = os.path.join('/', 'results', 'pdfs', spider_name)
+            os.makedirs(folder_path, exist_ok=True)
 
-    def process_item(self, item, spider):
-        """Process items sent by the spider."""
+    def check_keywords(self, item, spider_name, base_pdf_path):
+        """Convert the pdf file to a python object and analyse it to find
+        keywords and section based on the section/keywords files provided.
+        """
 
         keep_pdf = self.settings['KEEP_PDF']
         download_only = self.settings['DOWNLOAD_ONLY']
         feed = self.settings['FEED_CONFIG']
-        pdf_path = ''.join(['./results/pdfs/', spider.name, '/'])
+        pdf_result_path = os.path.join(
+            os.path.curdir,
+            'results',
+            'pdfs',
+            spider_name,
+        )
 
         # Convert PDF content to text format
-        f = open('/tmp/' + item['pdf'], 'rb')
+        with open(base_pdf_path, 'rb') as f:
+            if download_only:
+                os.rename(
+                    ''.join(['/tmp/', item['pdf']]),
+                    ''.join([pdf_result_path, item['pdf']])
+                )
+                return item
 
-        if download_only:
-            os.rename(
-                ''.join(['/tmp/', item['pdf']]),
-                ''.join([pdf_path, item['pdf']])
+            self.logger.info(
+                'Processing: %s (%s)',
+                item['pdf'],
+                self.settings['PARSING_METHOD'],
             )
-            return item
 
-        self.logger.info('Processing: %s (%s)' % (
-            item['pdf'],
-            self.settings['PARSING_METHOD'],
-        ))
+            if self.settings['PARSING_METHOD'] == 'pdftotext':
+                pdf_file = parse_pdf_document_pdftxt(f)
+            else:
+                pdf_file = parse_pdf_document(f)
 
-        # Parse using pdftotext only if specified. Leads to more false positive
-        if self.settings['PARSING_METHOD'] == 'pdftotext':
-            pdf_file = parse_pdf_document_pdftxt(f)
-        else:
-            pdf_file = parse_pdf_document(f)
+            for keyword in self.section_keywords:
+                # Fetch references or other keyworded list
+                section = grab_section(pdf_file, keyword)
 
-        for keyword in self.section_keywords:
+                # Add references and PDF name to JSON returned file
+                # If no section matchs, leave the attribute undefined
+                if section:
+                    item['sections'][keyword.title()] = section
+
             # Fetch references or other keyworded list
-            section = grab_section(pdf_file, keyword)
+            keyword_dict = pdf_file.get_lines_by_keywords(
+                self.keywords,
+                self.settings['KEYWORDS_CONTEXT']
+            )
 
             # Add references and PDF name to JSON returned file
             # If no section matchs, leave the attribute undefined
-            if section:
-                item['sections'][keyword.title()] = section
-
-        # Fetch references or other keyworded list
-        keyword_dict = pdf_file.get_lines_by_keywords(
-            self.keywords,
-            self.settings['KEYWORDS_CONTEXT']
-        )
-
-        # Add references and PDF name to JSON returned file
-        # If no section matchs, leave the attribute undefined
-        if keyword_dict:
-            item['keywords'] = keyword_dict
-
-        f.close()
+            if keyword_dict:
+                item['keywords'] = keyword_dict
 
         has_keywords = len(item['keywords'])
 
@@ -92,10 +99,22 @@ class WsfScrapingPipeline(object):
             else:
                 os.rename(
                     ''.join(['/tmp/', item['pdf']]),
-                    ''.join([pdf_path, item['pdf']])
+                    ''.join([pdf_result_path, item['pdf']])
                 )
         else:
-            os.remove('/tmp/' + item['pdf'])
-        insert_article(item['title'], item['uri'])
-
+            os.remove(base_pdf_path)
         return item
+
+    def process_item(self, item, spider):
+        """Process items sent by the spider."""
+        base_pdf_path = os.path.join('/', 'tmp', item['pdf'])
+        file_hash = get_file_hash(base_pdf_path)
+        if self.database.is_scraped(file_hash):
+            # File is already scraped in the database
+            raise DropItem(
+                'Item footprint is already in the database'
+            )
+        full_item = self.check_keywords(item, spider.name, base_pdf_path)
+        self.database.insert_article(item['title'], file_hash, item['uri'])
+
+        return full_item
