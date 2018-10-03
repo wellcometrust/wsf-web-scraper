8b71fd474f87f62e024bb205140dbc2a3d374019
SamDepardieu <sam.depardieu@gmail.com>
2018-10-03 11:53:21 +0100

    :art: Better item representation (text field flooded the logs)

--- wsf_scraping/items.py
+++ wsf_scraping/items.py
@@ -3,6 +3,13 @@ import scrapy
 
 
 class BaseArticle(scrapy.Item):
+    def __repr__(self):
+        return repr({
+            'title': self['title'],
+            'uri': self['uri'],
+            'provider': self['provider'],
+        })
+
     title = scrapy.Field()
     year = scrapy.Field()
     uri = scrapy.Field()
